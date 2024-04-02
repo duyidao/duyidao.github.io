e3da5d385962555f31a11ca9e6b5a24cd7c12da2
v_duyilin <v_duyilin@baidu.com>
2024-04-02 18:43:41 +0800

    面试典目录结构调整

--- docs/.vitepress/silber.js
+++ docs/.vitepress/silber.js
@@ -1077,20 +1077,36 @@ const DictionarySidebar = [
     collapsible: true,
     items: [
       {
-        text: "(a==1&&a==2&&a==3)",
-        link: "/面试鸭/面试典/(a==1&&a==2&&a==3)",
+        text: "Javascript",
+        collapsible: true,
+        items: [
+          {
+            text: "(a==1&&a==2&&a==3)",
+            link: "/面试鸭/面试典/js/(a==1&&a==2&&a==3)",
+          },
+          {
+            text: "数组方法手写原理",
+            link: "/面试鸭/面试典/js/数组方法手写原理",
+          },
+          {
+            text: "关于forEach使用break跳出循环",
+            link: "/面试鸭/面试典/js/关于forEach使用break跳出循环",
+          },
+          { text: "手写封装AJAX", link: "/面试鸭/面试典/js/手写封装AJAX" },
+          { text: "数组去重", link: "/面试鸭/面试典/js/数组去重" },
+          { text: "js执行顺序", link: "/面试鸭/面试典/js/js执行顺序" },
+        ]
       },
       {
-        text: "数组方法手写原理",
-        link: "/面试鸭/面试典/数组方法手写原理",
+        text: "项目",
+        collapsible: true,
+        items: [
+          {
+            text: "前端面试项目亮点和难点",
+            link: "/面试鸭/面试典/project/前端面试项目亮点和难点",
+          },
+        ]
       },
-      {
-        text: "关于forEach使用break跳出循环",
-        link: "/面试鸭/面试典/关于forEach使用break跳出循环",
-      },
-      { text: "手写封装AJAX", link: "/面试鸭/面试典/手写封装AJAX" },
-      { text: "数组去重", link: "/面试鸭/面试典/数组去重" },
-      { text: "js执行顺序", link: "/面试鸭/面试典/js执行顺序" },
     ],
   },
 ]
