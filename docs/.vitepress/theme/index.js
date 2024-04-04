3b7e1b8953eb766a1038a766a5f8fb102005bd9e
v_duyilin <v_duyilin@baidu.com>
2024-04-05 00:31:45 +0800

    样式调整

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,6 +1,8 @@
 import DefaultTheme from 'vitepress/theme'
 import MyComponent from '../components/MyComponent.vue';
 import Link from '../components/pseudo/link.vue';
+import Element from '../components/pseudo/element.vue';
+import Input from '../components/pseudo/input.vue';
 import './custom.less'
 
 export default {
@@ -8,5 +10,7 @@ export default {
     enhanceApp({ app }) {
         app.component('MyComponent', MyComponent);
         app.component('Link', Link);
+        app.component('Element', Element);
+        app.component('Input', Input);
     }
 }
