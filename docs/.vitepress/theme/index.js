65c38a7834f61716d7ea469ab0be27582deb3f7c
v_duyilin <v_duyilin@baidu.com>
2024-04-06 15:16:51 +0800

    暗黑模式样式调整，css伪类文档修改

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,5 +1,4 @@
 import DefaultTheme from 'vitepress/theme'
-import MyComponent from '../components/MyComponent.vue';
 import Link from '../components/pseudo/link.vue';
 import Element from '../components/pseudo/element.vue';
 import Input from '../components/pseudo/input.vue';
@@ -9,7 +8,6 @@ import './custom.less'
 export default {
     ...DefaultTheme,
     enhanceApp({ app }) {
-        app.component('MyComponent', MyComponent);
         app.component('Link', Link);
         app.component('Element', Element);
         app.component('Input', Input);
