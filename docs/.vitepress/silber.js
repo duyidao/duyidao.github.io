060f4373700b291106e6fd818975d81f6da16ff7
v_duyilin <v_duyilin@baidu.com>
2025-03-10 21:00:51 +0800

    feat: 新增vitepress搜索功能和文档

--- docs/.vitepress/silber.js
+++ docs/.vitepress/silber.js
@@ -1244,6 +1244,7 @@ const VitePressSidebar = [
         items: [
           { text: "内部拓展", link: "/vitePress/Know" },
           { text: "评论功能拓展", link: "/vitePress/review" },
+          { text: "搜索", link: "/vitePress/algolia" },
         ]
       },
     ],
